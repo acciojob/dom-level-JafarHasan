2f70dadc878f3dcff0871b4672391692c78103ba
JafarHasan <159635643+JafarHasan@users.noreply.github.com>
2024-07-18 12:08:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function domLevel(elementId) {
+	const element=document.getElementById(elementId);
+	let cnt=0;
+	while(element){
+		cnt++;
+		element=element.parentElement
+	}
+	
+}
+const getLevel=domLevel('level');
+alert(`The level of the element is:${getLevel}`);
